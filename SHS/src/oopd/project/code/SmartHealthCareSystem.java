ba4de7515b03abe9a93e13f439991e4878b6de8f
sarosh <sarosh.coer@gmail.com>
2018-10-29 19:29:44 +0530

    db connection

---
+++ SHS/src/oopd/project/code/SmartHealthCareSystem.java
@@ -0,0 +1,17 @@
+package oopd.project.code;
+
+import oopd.project.database.DatabaseConnection;
+
+
+
+public class SmartHealthCareSystem {
+	public static DatabaseConnection db=new DatabaseConnection();
+	public static void main(String[] args) {
+		
+		
+		db.readDb("");
+	
+		 
+	}
+
+}
