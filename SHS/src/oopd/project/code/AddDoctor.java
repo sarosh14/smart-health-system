a09a51e2b89df00dbfceae9d10d330482e5e076f
vikash kumar pandey <vikascsepandey@gmail.com>
2018-10-31 20:25:02 +0530

    labels_updated

--- SHS/src/oopd/project/code/AddDoctor.java
+++ SHS/src/oopd/project/code/AddDoctor.java
@@ -17,6 +17,9 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import javax.swing.JLabel;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class AddDoctor {
 
@@ -25,9 +28,7 @@ public class AddDoctor {
 	private JTextField textField_3;
 	private JTextField textField_4;
 	private JTextField textField_5;
-	private JEditorPane dtrpnAddress;
 	private JTextField textField_6;
-	private JEditorPane dtrpnS;
 	private JComboBox comboBox;
 	
 	private JComboBox comboBox_1;
@@ -39,6 +40,14 @@ public class AddDoctor {
 	 */
 	static AddDoctor window = new AddDoctor();
 	private JLabel lblYyyymmdd;
+	private JLabel lblDoctorName;
+	private JLabel lblDob;
+	private JLabel lblGender;
+	private JLabel lblAddress;
+	private JLabel lblNewLabel;
+	private JLabel lblRank;
+	private JLabel lblOpdFees;
+	private JLabel lblSurgeon;
 	public void invoke() {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -66,104 +75,33 @@ public class AddDoctor {
 		frame = new JFrame("Add Doctor");
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);   
-		
-		//increse jframe size
-		//Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		//frame.setSize(screenSize.width,screenSize.height); 
-		
-		
-		JEditorPane dtrpnName = new JEditorPane();
-		dtrpnName.setText("1- Doctor Name");
-		dtrpnName.setBounds(32, 37, 107, 21);
-		dtrpnName.setEditable(false);
-		frame.getContentPane().add(dtrpnName);
-		
-		JEditorPane dtrpnDepartmentId = new JEditorPane();
-		dtrpnDepartmentId.setText("2- Department Id");
-		dtrpnDepartmentId.setBounds(32, 144, 107, 21);
-		frame.getContentPane().add(dtrpnDepartmentId);
-		dtrpnDepartmentId.setEditable(false);
-		
-		JEditorPane dtrpnRank = new JEditorPane();
-		dtrpnRank.setText("3- Rank");
-		dtrpnRank.setBounds(32, 169, 107, 21);
-		frame.getContentPane().add(dtrpnRank);
-		dtrpnRank.setEditable(false);
-		
-		JEditorPane dtrpnOpdFee = new JEditorPane();
-		dtrpnOpdFee.setText("4- OPD Fees");
-		dtrpnOpdFee.setBounds(32, 193, 107, 21);
-		frame.getContentPane().add(dtrpnOpdFee);
-		dtrpnOpdFee.setEditable(false);
-		
-		JEditorPane dtrpnDob = new JEditorPane();
-		dtrpnDob.setText("5- D.O.B");
-		dtrpnDob.setBounds(32, 53, 107, 21);
-		frame.getContentPane().add(dtrpnDob);
-		dtrpnDob.setEditable(false);
-		
-		JEditorPane dtrpnGender = new JEditorPane();
-		dtrpnGender.setText("6- Gender");
-		dtrpnGender.setBounds(32, 86, 107, 21);
-		frame.getContentPane().add(dtrpnGender);
-		dtrpnGender.setEditable(false);
-		
-		dtrpnAddress = new JEditorPane();
-		dtrpnAddress.setText("7- Address");
-		dtrpnAddress.setEditable(false);
-		dtrpnAddress.setBounds(32, 111, 107, 21);
-		frame.getContentPane().add(dtrpnAddress);
-		dtrpnAddress.setEditable(false);
-		
-		dtrpnS = new JEditorPane();
-		dtrpnS.setText("8- Surgeon");
-		dtrpnS.setBounds(32, 218, 107, 21);
-		frame.getContentPane().add(dtrpnS);
-		dtrpnS.setEditable(false);
 		
 		textField = new JTextField();
-		textField.setBounds(165, 37, 124, 19);
-		frame.getContentPane().add(textField);
 		textField.setColumns(10);
 		
 		textField_1 = new JTextField();
-		textField_1.setBounds(165, 55, 124, 19);
-		frame.getContentPane().add(textField_1);
 		textField_1.setColumns(10);
 		
 		textField_3 = new JTextField();
-		textField_3.setBounds(165, 146, 124, 19);
-		frame.getContentPane().add(textField_3);
 		textField_3.setColumns(10);
 		
 		//int deptid = Integer.parseInt(dept_id);
 		textField_4 = new JTextField();
-		textField_4.setBounds(165, 171, 124, 19);
-		frame.getContentPane().add(textField_4);
 		textField_4.setColumns(10);
 		
 		textField_5 = new JTextField();
-		textField_5.setBounds(165, 195, 124, 19);
-		frame.getContentPane().add(textField_5);
 		textField_5.setColumns(10);
 		//int fee = Integer.parseInt(fe);
 
 		textField_6 = new JTextField();
-		textField_6.setBounds(165, 115, 124, 19);
-		frame.getContentPane().add(textField_6);
 		textField_6.setColumns(10);
 				
 	
 		String gender[]={"Male","Female"};  
 		comboBox = new JComboBox(gender);
-		comboBox.setBounds(165, 83, 77, 24);
-		frame.getContentPane().add(comboBox);
 		
 		String sergeon[]= {"Yes","No"};
 		comboBox_1 = new JComboBox(sergeon);
-		comboBox_1.setBounds(165, 218, 52, 24);
-		frame.getContentPane().add(comboBox_1);
 		
 		btnNewButton = new JButton("Add");
 		btnNewButton.addActionListener(new ActionListener() {
@@ -207,12 +145,135 @@ public class AddDoctor {
 		        } 
 			}
 		});
-		btnNewButton.setBounds(32, 239, 61, 25);
-		frame.getContentPane().add(btnNewButton);
 		
 		lblYyyymmdd = new JLabel("* yyyy/MM/dd");
-		lblYyyymmdd.setBounds(328, 53, 92, 21);
-		frame.getContentPane().add(lblYyyymmdd);
+		
+		lblDoctorName = new JLabel("1- Doctor Name");
+		
+		lblDob = new JLabel("2- D.O.B");
+		
+		lblGender = new JLabel("3- Gender");
+		
+		lblAddress = new JLabel("4- Address");
+		
+		lblNewLabel = new JLabel("5- Department Id");
+		
+		lblRank = new JLabel("6- Rank");
+		
+		lblOpdFees = new JLabel("7- OPD Fees");
+		
+		lblSurgeon = new JLabel("8- Surgeon");
+		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(32)
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblDoctorName, GroupLayout.DEFAULT_SIZE, 111, Short.MAX_VALUE)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addComponent(lblDob, GroupLayout.DEFAULT_SIZE, 70, Short.MAX_VALUE)
+									.addGap(41)))
+							.addGap(37)
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(textField, GroupLayout.DEFAULT_SIZE, 127, Short.MAX_VALUE)
+								.addComponent(textField_1, GroupLayout.DEFAULT_SIZE, 133, Short.MAX_VALUE))
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(lblYyyymmdd, GroupLayout.DEFAULT_SIZE, 114, Short.MAX_VALUE)
+							.addGap(16))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(lblGender, GroupLayout.DEFAULT_SIZE, 112, Short.MAX_VALUE)
+							.addGap(41)
+							.addComponent(comboBox, 0, 87, Short.MAX_VALUE)
+							.addGap(178))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblSurgeon, GroupLayout.DEFAULT_SIZE, 107, Short.MAX_VALUE)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addComponent(btnNewButton, GroupLayout.DEFAULT_SIZE, 76, Short.MAX_VALUE)
+									.addGap(31)))
+							.addGap(41)
+							.addComponent(comboBox_1, 0, 57, Short.MAX_VALUE)
+							.addGap(213))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addComponent(lblOpdFees, GroupLayout.DEFAULT_SIZE, 102, Short.MAX_VALUE)
+									.addGap(41))
+								.addGroup(groupLayout.createSequentialGroup()
+									.addComponent(lblRank, GroupLayout.DEFAULT_SIZE, 91, Short.MAX_VALUE)
+									.addGap(52))
+								.addComponent(lblNewLabel, GroupLayout.DEFAULT_SIZE, 131, Short.MAX_VALUE)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addComponent(lblAddress, GroupLayout.DEFAULT_SIZE, 117, Short.MAX_VALUE)
+									.addGap(26)))
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+								.addComponent(textField_6)
+								.addComponent(textField_3, GroupLayout.DEFAULT_SIZE, 134, Short.MAX_VALUE)
+								.addComponent(textField_4, Alignment.LEADING)
+								.addComponent(textField_5, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 134, Short.MAX_VALUE))
+							.addGap(141)))
+					.addContainerGap())
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(37)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+								.addComponent(lblDoctorName)
+								.addComponent(textField))
+							.addGap(7)
+							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+								.addComponent(lblDob)
+								.addComponent(textField_1))
+							.addGap(1))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(25)
+							.addComponent(lblYyyymmdd, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE)))
+					.addGap(9)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(1)
+							.addComponent(lblGender))
+						.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(8)
+							.addComponent(lblAddress, GroupLayout.PREFERRED_SIZE, 17, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(textField_6, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+					.addGap(11)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblNewLabel)
+						.addComponent(textField_3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(6)
+							.addComponent(lblRank))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(textField_4, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+					.addGap(7)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(2)
+							.addComponent(lblOpdFees))
+						.addComponent(textField_5, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(4)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(5)
+							.addComponent(lblSurgeon)
+							.addGap(1)
+							.addComponent(btnNewButton))
+						.addComponent(comboBox_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+		);
+		frame.getContentPane().setLayout(groupLayout);
 		
 	          
 //	    
